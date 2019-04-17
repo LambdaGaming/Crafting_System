a367b92009d95ab5accb75a4af366b5f4e7db847
LambdaGaming <starwars18m@gmail.com>
2019-04-17 09:49:13 -0400

    Update cl_init.lua

--- lua/entities/crafting_table/cl_init.lua
+++ lua/entities/crafting_table/cl_init.lua
@@ -8,18 +8,34 @@ local function DrawItems( ent )
     local leftframe = vgui.Create( "DFrame" )
     leftframe:SetTitle( "Current items on the table:" )
     leftframe:SetSize( 500, 700 )
-    leftframe:Center()
+    leftframe:AlignLeft()
     leftframe:MakePopup()
+    leftframe:ShowCloseButton( false )
     local leftframescroll = vgui.Create( "DScrollPanel", leftframe )
     leftframescroll:Dock( FILL )
-    for i=1, #ent.CraftingItems() do
+    for k,v in pairs( ent.CraftingItems ) do
         
     end
 
     local rightframe = vgui.Create( "DFrame" )
-    rightframe:SetTitle( "Choose and item to craft:" )
+    rightframe:SetTitle( "Choose an item to craft:" )
     rightframe:SetSize( 500, 700 )
-    rightframe:
+    rightframe:AlignRight()
+    rightframe:MakePopup()
+    rightframe:ShowCloseButton( false )
+    local rightframescroll = vgui.Create( "DScrollPanel", rightframe )
+    rightframescroll:Dock( FILL )
+    for k,v in pairs( ent.CraftingTable ) do
+        
+    end
+    
+    local centerframe = vgui.Create( "DFrame" )
+    centerframe:SetSize( 300, 50 )
+    centerframe:Center()
+    centerframe:MakePopup()
+    centerframe:SetBackgroundColor( Color( 255, 255, 255, 0 ) )
+    centerframe:ShowCloseButton( false )
+    local craftbutton = vgui.Create(  )
 end
 
 net.Receive( "CraftingTableMenu", function( len, ply )
