58cac52d36a091180e2a89513b06b4722962eab4
LambdaGaming <starwars18m@gmail.com>
2019-05-11 10:56:36 -0400

    Changed some colors

--- lua/autorun/craft_config.lua
+++ lua/autorun/craft_config.lua
@@ -25,6 +25,12 @@ CRAFT_CONFIG_SHOULD_EXPLODE = true
 --Sound that plays when the table is destroyed, only plays when the explosion is disabled, you don't need to have math.random, I only added that for variety
 CRAFT_CONFIG_DESTROY_SOUND = "physics/metal/metal_box_break"..math.random( 1, 2 )..".wav"
 
+--Color of the menu background
+CRAFT_CONFIG_MENU_COLOR = Color( 49, 53, 61, 200 )
+
+--Color of the buttons
+CRAFT_CONFIG_BUTTON_COLOR = Color( 230, 93, 80, 255 )
+
 --Entities that are allowed to be placed on the table. Don't forget to configure the items in the shared.lua as well! Failure to do so will result in errors!
 CRAFT_CONFIG_ALLOWED_ENTS = {
     "sent_ball",
